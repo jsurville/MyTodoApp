13b031f6124382a07f5450432a21892ccbda6ada
SURVILLE Julien <julien.surville@externe.e-i.com>
2021-03-08 21:15:46 +0100

    Add Price class to value.component.ts

--- MyTodoApp/src/app/views/value/value.component.ts
+++ MyTodoApp/src/app/views/value/value.component.ts
@@ -1,6 +1,11 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 
+class Price {
+  id: number;
+  value: string;
+}
+
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
@@ -16,12 +21,13 @@ export class ValueComponent implements OnInit{
   constructor(
     private todoDataService: ApiService  ) {  }
 
-  apiValues: string[] = [];
+  apiValues: Price[] = [];
   public ngOnInit(): void  {
     this.todoDataService
       .get('values')
       .subscribe(values => {
-        this.apiValues = values as string[];
+        console.log(values);
+        this.apiValues = values as Price[];
       });
   }
 }
